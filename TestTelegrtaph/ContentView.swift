a5b265d15dff2d7bd0a713a2198b0912c4086f4a
satanworker <satanworker@gmail.com>
2022-04-21 15:23:40 +0400

    Initial Commit

---
+++ TestTelegrtaph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTelegrtaph
+//
+//  Created by Dima Petrov on 21.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestTelegrtaph/TestTelegrtaphApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTelegrtaphApp.swift
+//  TestTelegrtaph
+//
+//  Created by Dima Petrov on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTelegrtaphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
